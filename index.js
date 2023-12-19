1b0a45a7bffd0e57ca3f4b39eb46b35d90176e86
ericpkatz <ericpkatz@gmail.com>
2023-12-19 13:11:30 -0500

    set up api routes

--- index.js
+++ index.js
@@ -1,5 +1,83 @@
+const express = require('express');
+const app = express();
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db');
+
+app.use(express.json());
+
+
+app.get('/api/notes', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM notes
+      ORDER BY ranking DESC
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/notes/:id', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM notes
+      WHERE id = $1
+    `;
+    const response = await client.query(SQL, [ req.params.id ]);
+    res.send(response.rows[0]);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/notes/:id', async(req, res, next)=> {
+  try {
+    const SQL = `
+      DELETE FROM notes
+      WHERE id = $1
+    `;
+    await client.query(SQL, [ req.params.id ]);
+    res.sendStatus(204);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/notes', async(req, res, next)=> {
+  try {
+    const SQL = `
+      INSERT INTO notes(txt, ranking) VALUES($1, $2) RETURNING *
+    `;
+    const response = await client.query(SQL, [ req.body.txt, req.body.ranking]);
+    res.status(201).send(response.rows[0]);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.put('/api/notes/:id', async(req, res, next)=> {
+  try {
+    const SQL = `
+      UPDATE notes
+      SET txt = $1, ranking = $2, updated_at = now()
+      WHERE id = $3
+      RETURNING *
+    `;
+    const response = await client.query(SQL, [ req.body.txt, req.body.ranking, req.params.id]);
+    res.status(201).send(response.rows[0]);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 
 const init = async()=> {
@@ -23,6 +101,8 @@ const init = async()=> {
   `;
   await client.query(SQL);
   console.log('data seeded');
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
 };
 
 init();
